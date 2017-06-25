4c38aef89d590c211492a5a7de9c6953d233c667
Jonathan White <thinkdigital@think-digital-laptop.fios-router.home>
2017-06-25 16:10:23 -0700

    module upload

---
+++ repairdeskapi.py
@@ -0,0 +1,23 @@
+"""python wrapper for RepairDesk OpenAPI"""
+import json
+import requests
+
+#scriptwide variables
+base_url="https://api.repairdesk.co/api/web/v1/"
+
+#sample_url https://api.repairdesk.co/api/web/v1/customers?api_key=YOUR_KEY
+
+def set_api_key(key):
+    global api_key
+    global api_key_string
+    api_key = key
+    api_key_string = "?api_key=" + api_key
+    return True
+
+def get_api_key():
+    return api_key
+
+def get_customers():
+    url = base_url + "customers" + api_key_string
+    customers = requests.get(url)
+    return customers
