e7436349770307797da13780fc47eefdbfc59902
Jonathan White <thinkdigital@think-digital-laptop.fios-router.home>
2017-07-01 14:42:05 -0700

    June 29th

--- repairdeskapi.py
+++ repairdeskapi.py
@@ -18,13 +18,33 @@ def get_api_key():
     return api_key
 
 def get_customers():
+    """Returns a list of only the customers in the JSON string"""
     url = base_url + "customers" + api_key_string
     customers = requests.get(url)
-    return customers
+    return list(customers.json().values())[3]
 
 class Customer(object):
-    """A customer in the RD System"""
-
+    """A customer in the RD System:
+    Attributes are :
+                    fullName
+cid
+phone
+mobile
+address1
+address2
+postcode
+city
+state
+country
+email
+organization
+refered_by
+driving_licence
+contact_person
+tax_number
+network
+    """
+#todo: Create attributes for each value
     def __init__(self, name):
         self.name = name
 
