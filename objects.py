f14cab413d866807e56df5e59794ec6c063c533a
ThinkDigitalRepair <thinkdigitalrepair@gmail.com>
2017-07-01 14:42:05 -0700

    June 29th

---
+++ objects.py
@@ -0,0 +1,45 @@
+import logging
+
+
+class Customer(object):
+    """A customer in the RD System:
+    Attributes are :
+                    fullName
+cid
+phone
+mobile
+address1
+address2
+postcode
+city
+state
+country
+email
+organization
+refered_by
+driving_licence
+contact_person
+tax_number
+network
+    """
+    def __init__(self, customer):
+        """ fullName, cid, phone, mobile, address1, address2, postcode, city, state, country, email, orgonization,
+                 refered_by, driving_licence, contact_person, tax_number, network"""
+        self.phone = customer['phone']
+        self.fullname = customer['fullName']
+        self.cid = customer['cid']
+        self.phone = customer['phone']
+        self.mobile = customer['mobile']
+        self.address1 = customer['address1']
+        self.address2 = customer['address2']
+        self.postal_code = customer['postcode']
+        self.city = customer['city']
+        self.state = customer['state']
+        self.country = customer['country']
+        self.email = customer['email']
+        self.organization = customer['orgonization']
+        self.referred_by = customer['refered_by']
+        self.driving_license = customer['driving_licence']
+        self.contact_person = customer['contact_person']
+        self.tax_number = customer['tax_number']
+        self.network = customer['network']
--- repairdeskapi.py
+++ repairdeskapi.py
@@ -1,32 +1,41 @@
 """python wrapper for RepairDesk OpenAPI"""
 import json
 import requests
+import objects
 
-#scriptwide variables
-base_url="https://api.repairdesk.co/api/web/v1/"
+# script-wide variables
+base_url = "https://api.repairdesk.co/api/web/v1/"
+api_key, api_key_string = "", ""
+customers = []
 
-#sample_url https://api.repairdesk.co/api/web/v1/customers?api_key=YOUR_KEY
+
+# sample_url https://api.repairdesk.co/api/web/v1/customers?api_key=YOUR_KEY
 
 def set_api_key(key):
-    global api_key
-    global api_key_string
+    # type: (str) -> bool
+    """
+
+    :type key: str
+    :rtype: bool
+    """
+    global api_key, api_key_string
     api_key = key
     api_key_string = "?api_key=" + api_key
     return True
 
-def get_api_key():
-    return api_key
+
+def get_api_key(self):
+    return self.api_key
+
 
 def get_customers():
+    # type: () -> list[]
+    """Returns a list of only the customers in the JSON string.
+    :rtype: list
+    """
     url = base_url + "customers" + api_key_string
-    customers = requests.get(url)
+    customer_list = requests.get(url)
+    # print(list(customers.json().values())[1])
+    for c in list(customer_list.json().values())[1]:
+        customers.append(objects.Customer(c))
     return customers
-
-class Customer(object):
-    """A customer in the RD System"""
-
-    def __init__(self, name):
-        self.name = name
-
-    def say_name(self):
-        print(self.name)
